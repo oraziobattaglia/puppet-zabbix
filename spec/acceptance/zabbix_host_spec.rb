fc9d086f72f450f46cda275d35bcdf9c7dae32e6
Orazio Battaglia <orazio.battaglia@unibo.it>
2021-08-26 15:04:01 +0200

    Fix hash syntax in test3 context

--- spec/acceptance/zabbix_host_spec.rb
+++ spec/acceptance/zabbix_host_spec.rb
@@ -187,7 +187,7 @@ describe 'zabbix_host type', unless: default[:platform] =~ %r{(ubuntu-16.04|debi
           expect(test3['interfaces'][0]['type']).to eq('2')
         end
         it 'has a correct interface details configured' do
-          expect(test3['interfaces'][0]['details']).to include(:version => 2, :bulk => 0, :community => 'public')
+          expect(test3['interfaces'][0]['details']).to include(version: 2, bulk: 0, community: 'public')
         end
         it 'has a correct interface useip configured' do
           expect(test3['interfaces'][0]['useip']).to eq('0')
